712eb22e809edb70242f167af05c4428664a439f
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-06-06 22:16:06 +0900

    front side settings for registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 登録完了時にメール送信
+      UserMailer.with(to: @user.email, name: @user.name).welcome.deliver_now
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+end
