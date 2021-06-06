4cf04c7ae61dce7b204f12c1e0e64ba4729731a5
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-06-06 22:32:48 +0900

    update sending setting

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  # 上記だと実際のメール送信は行われない
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
